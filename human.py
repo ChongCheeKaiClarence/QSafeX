19b143b4bc717b9cc733c0c97f710a0d3cb4c633
ChongCheeKaiClarence <e0725313.u.nus.edu>
2024-05-28 18:00:11 +0800

    tried yolov10 weights

--- human.py
+++ human.py
@@ -5,17 +5,28 @@ import threading
 import torch
 import calibrateregions
 from ultralytics import YOLO
+from ultralytics import YOLOv10
 import cv2
 
-from ultralytics import YOLO
-
 # Load a model
-model = YOLO("yolov8x-oiv7.pt")  # load an official model
+model = YOLO("weights\snehilsanyal-constructionn-site-safety-ppe.pt")  # load an official model
 # model("input_media/black_screen.png")
 # model = YOLO("weights\mnist_cls.pt")  # load a custom model
 
 # Predict with the model
-results = model("input_media\humanDistance4.jpg", imgsz=1280, show=True)  # predict on an image
+results = model("input_media\humanDistance1.jpg", imgsz=1280, show=True, classes=[0, 5, 7 ])  # predict on an image
 
 for result in results:
     result.show()
+
+
+# # Load a model
+# model = YOLOv10("weights\yolov10-ppe-roboflow-28May.pt")  # load an official model
+# # model("input_media/black_screen.png")
+# # model = YOLO("weights\mnist_cls.pt")  # load a custom model
+
+# # Predict with the model
+# results = model("input_media\humanDistance1.jpg", imgsz=640)  # predict on an image
+
+# for result in results:
+#     result.show()
