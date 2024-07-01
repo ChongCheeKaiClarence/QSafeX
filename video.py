f8787a1eeed0d9c5d0f585c7915c435c8fbd5550
ChongCheeKaiClarence <e0725313.u.nus.edu>
2024-07-01 21:04:22 +0800

    Add new weights

--- video.py
+++ video.py
@@ -1,15 +1,28 @@
 import cv2
+import os
+
 from ultralytics import YOLO
 
 # Load the YOLOv8 model
-model = YOLO("weights\safety_shoe_3Jun_3.pt")
+model = YOLO("weights\ppev1.pt")
 
 # Open the video file
-video_path = "input_media\Hoistlift6.mp4"
+video_path = "input_media/Hoistlift29.mp4"
 cap = cv2.VideoCapture(video_path)
 
-display_width = 800
-display_height = 600
+# Get video information
+fps = cap.get(cv2.CAP_PROP_FPS)
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+
+# Output video writer
+output_dir = 'output_media'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+output_video_path = os.path.join(output_dir, 'annotated_detect_video.avi')
+out = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'XVID'), fps, (frame_width, frame_height))
 
 # Loop through the video frames
 while cap.isOpened():
@@ -17,26 +30,25 @@ while cap.isOpened():
     success, frame = cap.read()
 
     if success:
-        # Run YOLOv8 tracking on the frame, persisting tracks between frames
-        results = model.track(frame, persist=True, imgsz=1280, classes=[0, 5, 7])
+        # Run YOLOv8 inference on the frame
+        results = model(frame)
 
         # Visualize the results on the frame
         annotated_frame = results[0].plot()
 
-        # Resize the annotated frame to fit the display window
-        resized_frame = cv2.resize(annotated_frame, (display_width, display_height))
-
-        # Display the resized frame
-        cv2.imshow("YOLOv8 Tracking", resized_frame)
+        # Display the annotated frame
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
 
         # Break the loop if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
     else:
         # Break the loop if the end of the video is reached
-        print("Failed to read frame")
         break
+    
+    out.write(annotated_frame)
 
 # Release the video capture object and close the display window
 cap.release()
+out.release()
 cv2.destroyAllWindows()
